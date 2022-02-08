53dac973ba201ce2bad238f244391c36bf8b10d2
Kamil Kośmider <78200901+shepard153@users.noreply.github.com>
2022-02-08 13:18:14 +0100

    Update login.php

--- login.php
+++ login.php
@@ -163,7 +163,6 @@ if(isset($_POST['submit']))
         ?>
 
         <button class="w-100 btn btn-lg btn-primary" type="submit" name="submit">Zaloguj</button>
-        <p class="mt-5 mb-3 text-muted">&copy; RUG 2021</p>
       </form>
     </main>
   </body>
